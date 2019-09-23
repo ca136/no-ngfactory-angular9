277f59923c4fd261c3223486ae5d2a65550ff738
Caleb Amsden <camsden@shopstyle.com>
2019-09-23 15:41:54 -0700

    remove factory reference

--- server.ts
+++ server.ts
@@ -2,10 +2,14 @@
 import 'zone.js/dist/zone-node';
 import 'reflect-metadata';
 
-import { enableProdMode } from '@angular/core';
-
 import * as express from 'express';
+
+import { enableProdMode } from '@angular/core';
 import { join } from 'path';
+// Express Engine
+import { ngExpressEngine } from '@nguniversal/express-engine';
+// Import module map for lazy loading
+import { provideModuleMap } from '@nguniversal/module-map-ngfactory-loader';
 
 // Faster server renders w/ Prod mode (dev mode never needed)
 enableProdMode();
@@ -17,15 +21,11 @@ const PORT = process.env.PORT || 4000;
 const DIST_FOLDER = join(process.cwd(), 'dist');
 
 // * NOTE :: leave this as require() since this file is built Dynamically from webpack
-const { AppServerModuleNgFactory, LAZY_MODULE_MAP } = require('./dist/server/main');
+const { AppServerModule, LAZY_MODULE_MAP } = require('./dist/server/main');
 
-// Express Engine
-import { ngExpressEngine } from '@nguniversal/express-engine';
-// Import module map for lazy loading
-import { provideModuleMap } from '@nguniversal/module-map-ngfactory-loader';
 
 app.engine('html', ngExpressEngine({
-  bootstrap: AppServerModuleNgFactory,
+  bootstrap: AppServerModule,
   providers: [
     provideModuleMap(LAZY_MODULE_MAP)
   ]
